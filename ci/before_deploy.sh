e0f6e548f3fe30b916c028e5925a40e9678e0cce
maix0 <maix522@gmail.com>
2020-01-05 06:16:42 +0100

    aaaaaa

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -21,7 +21,7 @@ main() {
     cross rustc --bin klu --target $TARGET --release -- -C lto
 
     # TODO Update this to package the right artifacts
-    cp target/$TARGET/release/klu $stage/
+    cp target/$TARGET/release/klu* $stage/
 
     cd $stage
     tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
